32e6892c407e4ee66df0bbf511ffef94c5c1f4f4
Nikki Lin <nikkiylin@gmail.com>
2020-03-20 08:26:50 -0700

    added Smurf.js inside component

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Smurf = (name, age, height, id) => {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <h3>{age}</h3>
+            <h3>{height}</h3>
+        </div>
+    )
+}
+export default Smurf;
